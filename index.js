3999106b2ace14b90be7bde918551c56e6f29307
ArabboyKayumov <arabboykayumov@gmail.com>
2024-01-22 23:34:03 -0800

    location added

---
+++ index.js
@@ -0,0 +1,5 @@
+// console.log(Math.random()(10 * 100));
+// console.log(Number(23).toString(2));
+// console.log(Number.parseInt("Arabboy", 2));
+// console.log(parseFloat("Arabboy").toFixed(2));
+// console.log(Math.floor(Math.random() * 500));
