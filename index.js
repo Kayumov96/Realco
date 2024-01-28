d02dad94d00cf17a6b84ffa976aa463fa2baa41b
ArabboyKayumov <arabboykayumov@gmail.com>
2024-01-27 23:48:18 -0800

    feedback completed

--- index.js
+++ index.js
@@ -2,4 +2,11 @@
 // console.log(Number(23).toString(2));
 // console.log(Number.parseInt("Arabboy", 2));
 // console.log(parseFloat("Arabboy").toFixed(2));
-var a = 5=console.log(Math.floor(Math.random() + 100 ));
+// var a = 10;
+// var b = 30;
+
+// console.log(Math.floor(Math.random() * (b - a) + a));
+// console.log(Number.parseFloat("sh"));
+// console.log((3.265576).toFixed(3));
+console.log(0 / 0);
+console.log(typeof NaN);
