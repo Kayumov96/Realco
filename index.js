54c44463425079b8db0e8c006fd078188034e7c0
ArabboyKayumov <arabboykayumov@gmail.com>
2024-03-07 23:49:49 -0800

    nothing

--- index.js
+++ index.js
@@ -735,57 +735,58 @@
 // eight(minus(three())); // must return 5
 // six(dividedBy(two())); // must return 3
 
-function zero(operation) {
-  return operation ? operation(0) : 0;
-}
-function one(operation) {
-  return operation ? operation(1) : 1;
-}
-function two(operation) {
-  return operation ? operation(2) : 2;
-}
-function three(operation) {
-  return operation ? operation(3) : 3;
-}
-function four(operation) {
-  return operation ? operation(4) : 4;
-}
-function five(operation) {
-  return operation ? operation(5) : 5;
-}
-function six(operation) {
-  return operation ? operation(6) : 6;
-}
-function seven(operation) {
-  return operation ? operation(7) : 7;
-}
-function eight(operation) {
-  return operation ? operation(8) : 8;
-}
-function nine(operation) {
-  return operation ? operation(9) : 9;
-}
+// function zero(operation) {
+//   return operation ? operation(0) : 0;
+// }
+// function one(operation) {
+//   return operation ? operation(1) : 1;
+// }
+// function two(operation) {
+//   return operation ? operation(2) : 2;
+// }
+// function three(operation) {
+//   return operation ? operation(3) : 3;
+// }
+// function four(operation) {
+//   return operation ? operation(4) : 4;
+// }
+// function five(operation) {
+//   return operation ? operation(5) : 5;
+// }
+// function six(operation) {
+//   return operation ? operation(6) : 6;
+// }
+// function seven(operation) {
+//   return operation ? operation(7) : 7;
+// }
+// function eight(operation) {
+//   return operation ? operation(8) : 8;
+// }
+// function nine(operation) {
+//   return operation ? operation(9) : 9;
+// }
 
-function plus(righthand) {
-  return function (lefthand) {
-    return lefthand + righthand;
-  };
-}
-function minus(righthand) {
-  return function (lefthand) {
-    return lefthand - righthand;
-  };
-}
-function times(righthand) {
-  return function (lefthand) {
-    lefthand * righthand;
-  };
-}
-function dividedBy(righthand) {
-  return function (lefthand) {
-    return lefthand / righthand;
-  };
-}
+// function plus(righthand) {
+//   return function (lefthand) {
+//     return lefthand + righthand;
+//   };
+// }
+// function minus(righthand) {
+//   return function (lefthand) {
+//     return lefthand - righthand;
+//   };
+// }
+// function times(righthand) {
+//   return function (lefthand) {
+//     lefthand * righthand;
+//   };
+// }
+// function dividedBy(righthand) {
+//   return function (lefthand) {
+//     return lefthand / righthand;
+//   };
+// }
+// console.log(seven(times(five())));
 // ----------------------------------------
 // setTimeout(() => {
 //   console.log("1"); //fifth
@@ -851,3 +852,63 @@ function dividedBy(righthand) {
 // };
 
 // getFlowers();
+// ----------------------------------
+
+// function zero(operation) {
+//   return operation ? operation(0) : 0;
+// }
+// function one(operation) {
+//   return operation ? operation(1) : 1;
+// }
+// function two(operation) {
+//   return operation ? operation(2) : 2;
+// }
+// function three(operation) {
+//   return operation ? operation(3) : 3;
+// }
+// function four(operation) {
+//   return operation ? operation(4) : 4;
+// }
+// function five(operation) {
+//   return operation ? operation(5) : 5;
+// }
+// function six(operation) {
+//   return operation ? operation(6) : 6;
+// }
+// function seven(operation) {
+//   return operation ? operation(7) : 7;
+// }
+// function eight(operation) {
+//   return operation ? operation(8) : 8;
+// }
+// function nine(operation) {
+//   return operation ? operation(9) : 9;
+// }
+
+// function plus(rightOperand) {
+//   return function (leftOperand) {
+//     return leftOperand + rightOperand;
+//   };
+// }
+// function minus(rightOperand) {
+//   return function (leftOperand) {
+//     return leftOperand - rightOperand;
+//   };
+// }
+// function times(rightOperand) {
+//   return function (leftOperand) {
+//     return leftOperand * rightOperand;
+//   };
+// }
+// function dividedBy(rightOperand) {
+//   return function (leftOperand) {
+//     return Math.floor(leftOperand / rightOperand);
+//   };
+// }
+
+// // Test cases
+// console.log(seven(times(five()))); // 35
+// console.log(four(plus(nine()))); // 13
+// console.log(eight(minus(three()))); // 5
+// console.log(six(dividedBy(two()))); // 3
+// ----------------------------------
